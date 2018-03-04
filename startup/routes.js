126a22556cd60bb5b5556e5f6b2894aebb59d850
akgoode <akgoode@gmail.com>
2018-03-04 00:53:30 -0600

    adds cors

--- startup/routes.js
+++ startup/routes.js
@@ -4,8 +4,10 @@ const auth = require('../routes/auth');
 const error = require('../middleware/error');
 const helmet = require('helmet');
 const express = require('express');
+const cors = require('cors');
 
 module.exports = function (app) {
+    app.use(cors());
     app.use(helmet());
     app.use(express.json( { type: 'application/json' }));
     app.use(express.urlencoded({ extended: false }));
