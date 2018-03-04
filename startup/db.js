e396db050796868d1697063587d9995f8518366f
akgoode <akgoode@gmail.com>
2018-03-03 23:42:40 -0600

    adds db var

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,9 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
 
+const db = process.env.MONGODB_URI || 'mongodb://localhost/srcollections-development';
+
 module.exports = function () {
-    mongoose.connect('mongodb://localhost/srcollections-development')
+    mongoose.connect(db)
     .then(() => winston.info('connected to mongodb...'));
 }
